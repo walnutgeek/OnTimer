49d6255434d9667f3a7663e4c7f53e073cf70964
Walnut Geek <wg@walnutgeek.com>
2014-08-17 01:08:47 -0700

    get rid of runner module

--- ontimer/tests/test_runner.py
+++
@@ -1,8 +0,0 @@
-import ontimer.runner
-from nose.tools import eq_
-
-def test_parseCmdLine(): 
-    l = list(ontimer.runner.parseCmdLine('a b  -c " dkfk dkjgkz" f \'slsls\' ')) 
-    eq_( ['a', 'b', '-c', ' dkfk dkjgkz', 'f', 'slsls'] , l )
-
-    
