98945d5ecbcc5f0bf480d164b70b06977ab1121f
Walnut Geek <wg@walnutgeek.com>
2014-08-25 08:51:50 -0700

    bugfix: extra th added in tbody on any update

--- debug.py
+++ debug.py
@@ -1,10 +1,11 @@
 from ontimer.tests.test_db import *
 import time
 import ontimer.event
+from ontimer import utils
 test_dbcreation()
 test_set_global_var()
 test_emit()
 events,tasks=dao.get_event_tasks()
-
+print utils.utc_adjusted(hours=-6)
           
             
