418d9bf9669b36096ec0617c7d90ec186222b504
Walnut Geek <wg@walnutgeek.com>
2014-10-12 23:13:16 -0700

    key_group_value map

--- ontimer/tests/test_utils.py
+++ ontimer/tests/test_utils.py
@@ -12,3 +12,125 @@ def test_utc_day_adjusted():
     eq_(adjhour.hour,adjmin.hour)
     eq_(adjhour.minute,adjmin.minute)
     eq_(adjhour.second,adjmin.second)
+
+def test_key_group_value():
+    kgv = utils.key_group_value()
+    
+    kgv.put('k1','g1', 'v1')
+    kgv.put('k2','g1', 'v2')
+    kgv.put('k1','g2', 'v3')
+    
+    eq_(kgv.get('k1','g1'),'v1')
+    eq_(kgv.get('k2','g1'),'v2')
+    eq_(kgv.get('k1','g2'),'v3')
+    
+    try:
+        kgv.get('k','g')
+        eq_(1,0) #fail
+    except KeyError:
+        pass
+    
+    eq_(kgv.keys_by_group('g1'),set(['k1','k2']))
+    eq_(kgv.keys_by_group('g2'),set(['k1']))
+    eq_(kgv.keys(),['k2', 'k1'])
+    
+    kgv.delete_key('k1')
+    
+    eq_(kgv.get('k2','g1'),'v2')
+    eq_(kgv.keys_by_group('g1'),set(['k2']))
+    eq_(kgv.keys(),['k2'])
+    
+    kgv.put('k1','g1', 'v1')
+    kgv.put('k1','g2', 'v3')
+    
+    kgv.delete_group('g1')
+    
+    eq_(kgv.get('k1','g2'),'v3')
+    eq_(kgv.keys_by_group('g2'),set(['k1']))
+    eq_(kgv.keys(),['k1'])
+    eq_(kgv.get_groups('k1'), {'g2': 'v3'})
+    
+    kgv.put('k1','g1', 'v1')
+    kgv.put('k2','g1', 'v2')
+    
+    kgv.delete_key_group('k1', 'g1')
+    eq_(kgv.keys(),['k2', 'k1'])
+
+    kgv.delete_key_group('k1', 'g2')
+    eq_(kgv.keys(),['k2'])
+    
+def test_kval_in_key_group_value():
+    kgv = utils.key_group_value(lambda k: k+'_')
+    
+    kgv.put('k1','g1', 'v1')
+    kgv.put('k2','g1', 'v2')
+    kgv.put('k1','g2', 'v3')
+    
+    eq_(kgv.get('k1','g1'),'v1')
+    eq_(kgv.get('k2','g1'),'v2')
+    eq_(kgv.get('k1','g2'),'v3')
+    
+    try:
+        kgv.get('k','g')
+        eq_(1,0) #fail
+    except KeyError:
+        pass
+    
+    eq_(kgv.keys_by_group('g1'),set(['k1','k2']))
+    eq_(kgv.keys_by_group('g2'),set(['k1']))
+    eq_(kgv.keys(),['k2', 'k1'])
+    eq_(kgv.get_kval('k2'),'k2_')
+    eq_(kgv.get_kval('k1'),'k1_')
+    
+    kgv.delete_key('k1')
+    
+    eq_(kgv.get('k2','g1'),'v2')
+    eq_(kgv.keys_by_group('g1'),set(['k2']))
+    eq_(kgv.keys(),['k2'])
+    eq_(kgv.get_kval('k2'),'k2_')
+    
+    try:
+        kgv.get_kval('k1')
+        eq_(1,0) #fail
+    except KeyError:
+        pass
+    
+    kgv.put('k1','g1', 'v1')
+    kgv.put('k1','g2', 'v3')
+    
+    kgv.delete_group('g1')
+
+    eq_(kgv.get_kval('k1'),'k1_')
+    try:
+        kgv.get_kval('k2')
+        eq_(1,0) #fail
+    except KeyError:
+        pass
+    
+    eq_(kgv.get('k1','g2'),'v3')
+    eq_(kgv.keys_by_group('g2'),set(['k1']))
+    eq_(kgv.keys(),['k1'])
+    eq_(kgv.get_groups('k1'), {'g2': 'v3'})
+    
+    kgv.put('k1','g1', 'v1')
+    kgv.put('k2','g1', 'v2')
+    
+    kgv.delete_key_group('k1', 'g1')
+    eq_(kgv.keys(),['k2', 'k1'])
+
+    eq_(kgv.get_kval('k2'),'k2_')
+    eq_(kgv.get_kval('k1'),'k1_')
+    
+    kgv.delete_key_group('k1', 'g2')
+    eq_(kgv.keys(),['k2'])
+
+    eq_(kgv.get_kval('k2'),'k2_')
+    
+    try:
+        kgv.get_kval('k1')
+        eq_(1,0) #fail
+    except KeyError:
+        pass
+
+    
+    
--- ontimer/utils.py
+++ ontimer/utils.py
@@ -3,8 +3,9 @@ Created on Jul 11, 2014
 
 @author: sergeyk
 '''
-import pytz
 import datetime
+from collections import defaultdict
+
 
 format_Y_m_d_H_M_S = '%Y-%m-%d %H:%M:%S'
 format_Ymd_HMS     = '%Y%m%d-%H%M%S'
@@ -20,6 +21,8 @@ all_formats = [format_Y_m_d_H_M_S,
 
 def toDateTime(s,formats=all_formats):
     '''try different date formats to parse string'''
+    if isinstance(s,datetime.datetime):
+        return s
     for f in formats:
         try:
             return datetime.datetime.strptime(s, f)
@@ -36,3 +39,107 @@ def quict(**kwargs):
     return kwargs
 
 
+class KeyEqMixin:
+    def __hash__(self):
+        return hash(self.__key__())
+    def __eq__(self,other):
+        return self.__key__() == other.__key__()
+    def __ne__(self,other):
+        return self.__key__() != other.__key__()
+
+class KeyCmpMixin(object):
+    def __lt__(self, other):
+        return self.__key__() < other.__key__()
+
+    def __gt__(self, other):
+        return other < self
+
+    def __le__(self, other):
+        return not (other < self)
+
+    def __ge__(self, other):
+        return not (self < other)
+
+
+class key_group_value:
+    def __init__(self,kval_producer = None):
+        self.kval_producer = kval_producer
+        self.key_map = {}
+        self.key_group_map = {}
+        self.inverse = None
+
+    def keys(self):
+        return self.key_group_map.keys()
+
+    def keys_by_group(self,group):
+        return self._inverse()[group]
+    
+    def get_groups(self, key):
+        return self.key_group_map[key]
+ 
+    def contains(self, key, group):
+        return key in self.key_group_map and group in self.key_group_map[key]
+        
+    def get(self, key, group):
+        return self.key_group_map[key][group]
+    
+    def put(self, key, group, value):
+        if key not in self.key_group_map:
+            self.key_group_map[key]={ group : value }
+            if self.kval_producer:
+                self.key_map[key]=self.kval_producer(key)
+        else:
+            self.key_group_map[key][group]=value
+        self.inverse = None
+
+    def get_kval(self, key):
+        return self.key_map[key]
+    
+    def has_kval(self, key):
+        return key in self.key_map
+    
+    def set_kval(self, key, kval):
+        if key not in self.key_group_map:
+            raise KeyError("%r key not in key_group_map" % key)
+        self.key_map[key] = kval
+        
+    def del_kval(self, key):
+        del self.key_map[key]
+        
+    def _inverse(self):
+        if None == self.inverse:
+            inverse = defaultdict(set)
+            for k in self.key_group_map:
+                for g in self.key_group_map[k]:
+                    inverse[g].add(k)
+            self.inverse = inverse
+        return self.inverse
+        
+
+    def delete_key(self, key):
+        del self.key_group_map[key]
+        if key in self.key_map: 
+                del self.key_map[key]
+        self.inverse = None
+
+    def _delete_key_group(self, key, group):
+        del self.key_group_map[key][group]
+        if len(self.key_group_map[key]) == 0:
+            del self.key_group_map[key]
+            if key in self.key_map: 
+                del self.key_map[key]
+            
+
+    def delete_key_group(self, key, group):
+        self._delete_key_group(key, group)
+        self.inverse = None
+    
+    def delete_group(self, group):
+        keys = self.keys_by_group(group)
+        for key in keys:
+            self._delete_key_group(key, group)
+        self.inverse = None
+    
+            
+        
+        
